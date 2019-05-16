183b86f8341757b5d5f2b1c216d1c8471782bc26
l1nker4 <l1nker4@outlook.com>
2019-05-16 10:28:03 +0800

    add new features

--- src/main/java/wang/l1n/dormitory/controller/MalfunctionController.java
+++ src/main/java/wang/l1n/dormitory/controller/MalfunctionController.java
@@ -3,7 +3,11 @@ package wang.l1n.dormitory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import wang.l1n.dormitory.dto.BaseResult;
 import wang.l1n.dormitory.entity.Malfunction;
 import wang.l1n.dormitory.service.MalfunctionService;
 
@@ -26,6 +30,17 @@ public class MalfunctionController {
         List<Malfunction> malfunctions = malfunctionService.getMalfunctionList();
         model.addAttribute("malfunctions", malfunctions);
         return "malfunction/list";
+    }
+
+    @RequestMapping(value = "/detail/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public BaseResult getDetail(@PathVariable("id") int id){
+        BaseResult baseResult = new BaseResult();
+        Malfunction malfunction = malfunctionService.getMalfunctionDetail(id);
+        baseResult.setStatus(200);
+        baseResult.setMessage("查询成功");
+        baseResult.getMap().put("malfunction", malfunction);
+        return baseResult;
     }
 
 }
